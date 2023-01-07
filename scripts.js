8fb61eca14ddfeaf0d5d8ab9eae989c58db75124
angelolustosa <angelolustosa@gmail.com>
2023-01-06 21:53:26 -0300

    aula 1 de logica com javascript

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// document.write("<h1>Hello World</h1>");
+
+// document.getElementById("container").innerHTML = "<h1>Hello World da esquerda</h1>";
+// document.querySelector("#container").innerHTML = "<h2>Outra coisa</h2>";
+
+// let paragrafo = document.createElement("p");
+// paragrafo.append("Inserido com Append()");
+// document.getElementById("container").appendChild(paragrafo);
+
+function somar()
+{
+    let resultado = Number(valor1.value) + Number(valor2.value);
+    document.getElementById("resultado").innerHTML = "<h4>Resultado é: "+resultado+"</h4>";
+}
+
+function subtrair()
+{
+    let resultado = Number(valor1.value) - Number(valor2.value);
+    document.getElementById("resultado").innerHTML = "<h4>Resultado é: "+resultado+"</h4>";
+}
+
+function multiplicar()
+{}
+
+function dividir()
+{}
