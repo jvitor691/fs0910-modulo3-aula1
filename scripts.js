138dfe674b8f8201fbd00f1cfb40a6c08dcfbddc
jvitor691 <jvitormarques2@hotmail.com>
2023-01-10 22:29:49 -0300

    Acrescimo de novas operações

--- scripts.js
+++ scripts.js
@@ -20,7 +20,13 @@ function subtrair()
 }
 
 function multiplicar()
-{}
+{
+    let resultado = Number(valor1.value) * Number(valor2.value);
+    document.getElementById("resultado").innerHTML = "<h4>Resultado é: "+resultado+"</h4>" 
+}
 
 function dividir()
-{}
+{
+    let resultado = Number(valor1.value) / Number(valor2.value);
+    document.getElementById("resultado").innerHTML = "<h4>Resultado é: "+resultado+"</h4>"
+}
